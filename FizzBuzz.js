fbec02927f7ebe20f24f6680480013607a50b92d
Tommi <mato12@hotmail.fi>
2017-09-14 21:13:03 +0300

    Implemented FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,10 @@
+const isDividedBy = (x,n) => string => x % n === 0 ? string : '';
+
+const fb = (number) => isDividedBy(number,3)('Fizz') + ' ' + isDividedBy(number,5)('Buzz');
+
 export default function* fizzBuzz() {
   var current = 1;
   do {
-    yield current++;
+    yield fb(current++).trim();
   } while(true);
 };
